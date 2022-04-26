d27a8d1ba23dde1d93f31c7cc1974cbb9892c473
Rachel Michaela <hacking@rami.org.il>
2022-04-25 19:15:44 -0500

    Fix broken variable definition for Emacs theme.

--- dotfiles/emacs/config.el
+++ dotfiles/emacs/config.el
@@ -2,7 +2,7 @@
 ;;       user-mail-address "")
 (setq doom-font (font-spec :family "Ubuntu"
                            :size 13))
-(setq doom-theme 'doom-one')
+(setq doom-theme 'doom-one)
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
 (setq-default indent-tabs-mode t)
