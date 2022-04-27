bf62863ae312c2bd69f52ca09d989d463e54fdba
Rachel Michaela <hacking@rami.org.il>
2022-04-26 19:01:56 -0500

    Switch Emacs theme and fix font name.

--- dotfiles/emacs/config.el
+++ dotfiles/emacs/config.el
@@ -1,8 +1,8 @@
 ;; (setq user-full-name ""
 ;;       user-mail-address "")
-(setq doom-font (font-spec :family "Ubuntu"
+(setq doom-font (font-spec :family "Ubuntu Mono"
                            :size 13))
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-manegarm)
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
 (setq-default indent-tabs-mode t)
