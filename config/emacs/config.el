cedd0d06be66e3be7b57c861cd3c6dd5bc8fdba9
Rachel Michaela <hacking@rami.org.il>
2022-10-17 10:39:16 -0500

    change: Use default Doom theme

--- config/emacs/config.el
+++ config/emacs/config.el
@@ -1,6 +1,6 @@
 (setq user-full-name "Rachel Michaela"
       user-mail-address "hacking@rami.org.il")
-(setq doom-theme 'doom-manegarm)
+(setq doom-theme 'doom-one)
 (setq display-line-numbers-type t)
 (setq org-directory "~/org/")
 (setq-default indent-tabs-mode t)
