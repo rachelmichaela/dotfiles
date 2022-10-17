f362ebe243eb320f40777b0841f62012b22db9df
Rachel Michaela <hacking@rami.org.il>
2022-10-17 10:43:50 -0500

    change: Reorganize and include comments

--- config/emacs/config.el
+++ config/emacs/config.el
@@ -1,8 +1,18 @@
+;;; General configuration
+;; - Define identity for Git et al
+;; - Set Doom theme to default
+;; - Set local org file directory to home subdirectory
+;; - Disable exit confirmation messages
 (setq user-full-name "Rachel Michaela"
       user-mail-address "hacking@rami.org.il")
 (setq doom-theme 'doom-one)
-(setq display-line-numbers-type 'relative)
 (setq org-directory "~/org/")
-(setq-default indent-tabs-mode t)
 (setq confirm-kill-emacs nil)
+
+;;; Editor Configuration
+;; - Use relative line numbers
+;; - Use tabs for indentation
+;; - Provide an 80-column ruler
+(setq display-line-numbers-type 'relative)
+(setq-default indent-tabs-mode t)
 (global-display-fill-column-indicator-mode t)
