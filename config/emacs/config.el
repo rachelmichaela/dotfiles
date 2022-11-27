0a1352c5bbf5fc1ba4d2c587fd966388aeed82b9
Rachel Michaela <hacking@rami.org.il>
2022-11-27 15:18:50 -0600

    add: Improve sane indentation support

--- config/emacs/config.el
+++ config/emacs/config.el
@@ -16,16 +16,25 @@
 ;;; Editor Configuration
 ;; - Use relative line numbers
 ;; - Use tabs for indentation
+;; - Use a tab width of 4 columns
 ;; - Provide an 80-column ruler
+;; - Add modes to EditorConfig indentation list
 (setq display-line-numbers-type 'relative)
 (setq-default indent-tabs-mode t)
+(setq-default tab-width 4)
 (global-display-fill-column-indicator-mode t)
+(after! editorconfig
+  (add-to-list 'editorconfig-indentation-alist
+	       '(cperl-mode cperl-indent-level))
+  (add-to-list 'editorconfig-indentation-alist
+	       '(LaTeX-mode latex-indent-level)))
 
 ;;; Perl Mode
 ;; - Enforce cperl-mode
 ;; - Use sane hash block indentation
 ;; - Enable cperl in flycheck
 ;; - Default CGI, PCGI, cpanfile and test files to Perl
+;; - Explicitly enforce tab indentation
 (fset 'perl-mode 'cperl-mode)
 (setq cperl-indent-parens-as-block t)
 (add-hook 'cperl-mode-hook 'flycheck-mode)
@@ -33,7 +42,9 @@
 (setq auto-mode-alist (cons '("\\.cgi$" . perl-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.psgi$" . perl-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.t$" . perl-mode) auto-mode-alist))
+(setq cperl-indent-level 4)
+(setq cperl-tab-always-indent t)
 
 ;;; Markdown Mode
-;; - Add Template Toolkit files to markdown-mode
+;; - Add template toolkit files to markdown mode
 (setq auto-mode-alist (cons '("\\.tt$" . markdown-mode) auto-mode-alist))
